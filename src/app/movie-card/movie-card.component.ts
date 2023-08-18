74ac707d5453aced59a140009af9b2cfbbddb63c
JarJarDinks <Cochranejacob22@gmail.com>
2023-08-17 21:50:06 -0700

    repo transfer init

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss'],
+})
+export class MovieCardComponent {
+  movies: any[] = [];
+  constructor(public fetchApiData: FetchApiDataService) {}
+
+  //ngOnInit is called when Angular is done creating the component
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+}
