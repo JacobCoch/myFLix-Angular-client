24fb159e50630842a7b11365eb34f3a9d883afc7
JarJarDinks <Cochranejacob22@gmail.com>
2023-08-21 16:15:38 -0700

    replace local repository

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myFlix-Client-Angular'`, () => {
+  it(`should have as title 'myflix-angular-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myFlix-Client-Angular');
+    expect(app.title).toEqual('myflix-angular-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myFlix-Client-Angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('myflix-angular-client app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myFlix-Client-Angular';
+  title = 'myflix-angular-client';
 }
