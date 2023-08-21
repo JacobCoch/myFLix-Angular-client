8524fb2f02db09424890c46fdb59f3b0899ce5de
JarJarDinks <Cochranejacob22@gmail.com>
2023-08-21 16:32:05 -0700

    adding all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent,
+    UserLoginFormComponent,
+    UserRegistrationFormComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
