86df06feb0c7c959f4c1223a16ff6e32ae4d7f9a
JarJarDinks <Cochranejacob22@gmail.com>
2023-08-23 18:22:16 -0700

    add details view component

--- src/app/details-view/details-view.component.ts
+++ src/app/details-view/details-view.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-details-view',
   templateUrl: './details-view.component.html',
-  styleUrls: ['./details-view.component.scss']
+  styleUrls: ['./details-view.component.scss'],
 })
-export class DetailsViewComponent {
+export class DetailsViewComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Title: string;
+      Description: string;
+    }
+  ) {}
 
+  ngOnInit(): void {}
 }
